845fcfdc4bbbcb3319b2ce6ca7f3abd39b950033
HypnoMama <sarahhermsen@gmail.com>
2018-07-06 19:55:05 +0000

    created functions in modThing and exported to index

---
+++ creating-modules/index.js
@@ -0,0 +1,15 @@
+var modThing = require('./modThing');
+
+
+modThing.storeNumber(7);
+modThing.storeNumber(37)
+modThing.storeNumber(10)
+modThing.storeNumber(7)
+
+modThing.storeNumber(125)
+
+console.log(modThing.storeNumber(2))
+
+
+
+
---
+++ creating-modules/modThing.js
@@ -0,0 +1,20 @@
+
+var numList = [];
+sortData = function(ary) {
+    ary.sort(function(a, b) {
+      return a - b;
+    })
+    return ary;
+  }
+
+module.exports = {
+
+  storeNumber: function(num) {
+    numList.push(num);
+    if (numList.length > 1) {
+      return sortData(numList);
+    } return numList;
+
+  }
+
+}
